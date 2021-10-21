e3da6fe36d95aafcc43fe953be68310a28a77fb3
RahidDev <rkhan14702@gmail.com>
2021-10-21 12:17:40 +0100

    Enter

--- example.js
+++ example.js
@@ -1,28 +1,3 @@
 let Days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
-let dayNumber = 6;
-
-if (dayNumber === 0) {
-  console.log("Sunday");
-}
-else if (dayNumber === 1) {
-  console.log("Monday");
-}
-else if (dayNumber === 2) {
-  console.log("Tuesday");
-}
-else if (dayNumber === 3) {
-  console.log("Wednesday");
-}
-else if (dayNumber === 4) {
-  console.log("Thursday");
-}
-else if (dayNumber === 5) {
-  console.log("Friday");
-}
-else if (dayNumber === 6) {
-  console.log("Saturday");
-}
-else {
-  console.log("Incorrect day number")
-} 
+console.log(Days);
