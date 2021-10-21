1c20257474fce9bd1a8634e395aed834b0a52cf1
RahidDev <rkhan14702@gmail.com>
2021-10-21 12:25:08 +0100

    enter

--- example.js
+++ example.js
@@ -1,3 +1,29 @@
-let dayNumber = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+let Days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
-alert('RIP');
+let dayNumber = 6;
+
+if (dayNumber === 0) {
+  console.log(Days[0]);
+}
+else if (dayNumber === 1) {
+    console.log(Days[1]);
+}
+else if (dayNumber === 2) {
+    console.log(Days[2]);
+}
+else if (dayNumber === 3) {
+    console.log(Days[3]);
+}
+else if (dayNumber === 4) {
+    console.log(Days[4]);
+}
+else if (dayNumber === 5) {
+    console.log(Days[5]);
+}
+else if (dayNumber === 6) {
+    console.log(Days[6]);
+}
+else if (dayNumber > 6) 
+{
+  console.log("Incorrect day number")
+} 
