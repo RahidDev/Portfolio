707b2cc3df958f0ae27a3b189213ced508b004d5
RahidDev <rkhan14702@gmail.com>
2021-10-22 16:30:39 +0100

    Enter

--- example.js
+++ example.js
@@ -1,31 +1,44 @@
-let Days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+const numHorses = 5
+let horses = []
+let horseX = []
 
-let dayNumber = Math.floor(Math.random(0,6)*10);
-console.log(dayNumber)
-alert('CHOOSE A NUMBER PRICK');
+let colors = ["Red","Blue","Orange","Cyan","Mageenta","Yellow","Black","Turqouise","Gray","Lilac","White"]
 
-if (dayNumber === 0) {
-  console.log(Days[0]);
-}
-else if (dayNumber === 1) {
-    console.log(Days[1]);
-}
-else if (dayNumber === 2) {
-    console.log(Days[2]);
-}
-else if (dayNumber === 3) {
-    console.log(Days[3]);
-}
-else if (dayNumber === 4) {
-    console.log(Days[4]);
-}
-else if (dayNumber === 5) {
-    console.log(Days[5]);
-}
-else if (dayNumber === 6) {
-    console.log(Days[6]);
-}
-else if (dayNumber > 6) 
+function Start()
 {
-  console.log("Incorrect day number")
-} 
+    setInterval(move,50)
+}
+
+for(let i = 0;i<numHorses;h++)
+{
+
+    horses[i] = document.createElement("div")
+    horses[i].classList.add("horse")
+    horses[i].style.top = 50  + i*18 + "px"
+    horses[i].style.backgroundAttachment = colors[i]
+    horseX[i] = 0
+    document.body.appendChild(horses[i])
+}
+
+const finish = 500
+
+function move()
+{
+    for(let i = 0;i < numHorses;i++)
+    {
+        horses[i].style.let = horsesX[i] + "px"
+        horseX[i] += Math.floor(Math.random() * 10)
+        if(horseX[i] >= finish)
+        {
+            alert(` ${colors[i]} horse wins`);resetReset();
+        }
+    }
+}
+
+function resetReset()
+{
+    for(let i = 0; i < numHorses;i++)
+    {
+        horseX[i] = 0
+    }
+}
